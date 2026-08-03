7cd530c491612576046073a96472a5a517b22ab0
Crhystel <crhystelvelasco213@gmail.com>
2026-08-03 18:08:30 -0500

    feat: configurar CORS para permitir peticiones desde el   frontend local

--- delycattessen_backend/settings.py
+++ delycattessen_backend/settings.py
@@ -45,9 +45,11 @@ INSTALLED_APPS = [
     'punto_venta',
     'perfiles',
     'reportes',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 ROOT_URLCONF = 'delycattessen_backend.urls'
